1a7d344cfd553c11418ab7d4cfd2de18defe2371
Avik2293 <aviksarker22@gmail.com>
2022-10-11 11:18:57 +0600

    toastify add

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Statistics from './Statistics';
 
 const Navigation = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -6,15 +6,15 @@ const Statistics = () => {
     const quizNumberData = useContext(QuizTypeContext);
     // console.log(quizNumberData.data);
     return (
-        <div>
-            <LineChart width={370} height={300} data={quizNumberData.data} margin={{ top: 50, right: 5, bottom: 5, left: 0 }}>
+        <div className="flex flex-col items-center h-screen">
+            <LineChart width={370} height={300} data={quizNumberData.data} margin={{ top: 50, right: 5, bottom: 5, left: -30 }}>
                 <Line type="monotone" dataKey="total" stroke="#8884d8" />
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
             </LineChart>
-            <h4>Chart: Name of Quiz Type vs Quiz No. of each Type</h4>
+            <h4 className='center'>Chart: Name of Quiz Type vs Quiz No. of each Type</h4>
         </div>
     );
 };
